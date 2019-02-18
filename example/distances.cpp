11315f1f1368171af044a6c8161d8e0d28144024
Marcus Trautmann <Marcus.Trautmann@outlook.com>
2019-02-18 16:49:54 +0100

    Add test for parallel planes

--- example/distances.cpp
+++ example/distances.cpp
@@ -38,23 +38,35 @@ int main()
 {
 	std::cout << "Exmples for Analytical Geometry Library" << std::endl;
 
+
+
 	analyticalgeom::Coordinate p0(0, 1, 0);
 	analyticalgeom::Coordinate p1(2, 1, 0);
 	analyticalgeom::Coordinate p2(1, 2, 1);
-	analyticalgeom::Coordinate p3(10, 1, 15);
+	analyticalgeom::Coordinate p3(1, 2, 1.0000001);
+
 
 	std::cout << "The distance between " << p0 << " and " << p1 << " is " << (p1 - p0).length() << std::endl;
-	std::cout << "The distance between " << p0 << " and " << p2 << " is " << (p2-p0).length() << std::endl;
+	std::cout << "The distance between " << p0 << " and " << p2 << " is " << (p2-p0).length() << std::endl << std::endl;
 
 	analyticalgeom::Line line(p0, p1);
 	std::cout << "The line through point " << p0 << " and " << p1 << " is " << line << std::endl;
 	std::cout << "The projection of point " << p2 << " on line " << line << " is point " << analyticalgeom::AnalyticalGeometry::projection(line, p2) << std::endl;
-	std::cout << "The shortest distance between line " << line << " and point " << p2 << " is " << analyticalgeom::AnalyticalGeometry::distance(line, p2) << std::endl;
+	std::cout << "The shortest distance between line " << line << " and point " << p2 << " is " << analyticalgeom::AnalyticalGeometry::distance(line, p2) << std::endl << std::endl;
 
 	analyticalgeom::Plane plane(p0, p1, p2);
 	std::cout << "The points " << p0 << ", " << p1 << " and " << p2 << " form the plane " << plane << std::endl;
 	std::cout << "The projection of " << p3 << " on the plane " << plane << " is " << analyticalgeom::AnalyticalGeometry::projection(plane, p3) << std::endl;
-	std::cout << "The distance between point " << p3 << " and plane " << plane << " is " << analyticalgeom::AnalyticalGeometry::distance(plane, p3) << std::endl;
+	std::cout << "The distance between point " << p3 << " and plane " << plane << " is " << analyticalgeom::AnalyticalGeometry::distance(plane, p3) << std::endl << std::endl;
+
+	analyticalgeom::Plane plane2(p0 + plane.n(), plane.n());
+	std::cout << "The plane " << plane << " shifted in normal direction by 1 unit produces the plane " << plane2 << std::endl;
+	std::cout << "The planes are " << (analyticalgeom::AnalyticalGeometry::isParallel(plane, plane2) ? std::string("parallel") : std::string("not parallel")) << std::endl;
+
+	analyticalgeom::Plane plane3(p0, p1, p3);
+	std::cout << plane.n() * plane3.n() << std::endl;
+	std::cout << "The planes " << plane << " and " << plane3 << " are " << (analyticalgeom::AnalyticalGeometry::isParallel(plane, plane3) ? std::string("parallel") : std::string("not parallel")) << std::endl;
+
 
 	return 0;
 }
